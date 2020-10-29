5cde8a648bb02ac34d51ef64c2168ac0ae692dee
SkyShell <pupyshev.igor999999@mail.ru>
2020-10-29 12:44:53 +0400

    start

---
+++ AutoClicker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запись";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(136, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 155);
+            this.label1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(221, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 107);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Пуск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Сброс";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(273, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 68);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(273, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 68);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 320);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AutoClicker/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using WindowsInput;
+
+namespace AutoClicker
+{
+    public partial class Form1 : Form
+    {
+        int qwe = 0;
+        int CountClick = 700;
+        private readonly Timer tmrShow;
+        private readonly Timer ClickShow;
+        private readonly Timer TimeShow;
+        bool isPress = false;
+        bool isPressqwe = false;
+        List<int> numbersX = new List<int>();
+        List<int> numbersY = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tmrShow = new Timer();
+            tmrShow.Interval = 3000;
+            tmrShow.Tick += tmrShow_Tick;
+
+            ClickShow = new Timer();
+            ClickShow.Interval = 40000;
+            ClickShow.Tick += ClickShow_Tick;
+
+            TimeShow = new Timer();
+            TimeShow.Interval = 1;
+            TimeShow.Tick += Time_Tick;
+        }
+
+        private void tmrShow_Tick(object sender, EventArgs e)
+        {
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+
+            numbersX.Add(x);
+            numbersY.Add(y);
+
+            label1.Text = label1.Text + x + " " + y + "\n";
+            
+        }
+
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            if (isPress)
+            {
+                TimeShow.Interval = 1000;
+                if (qwe < tmrShow.Interval / 1000 + 1)
+                {
+                    label2.Text = (tmrShow.Interval / 1000 - qwe).ToString();
+                    qwe++;
+                }
+                else
+                {
+                    qwe = 1;
+                    TimeShow.Interval = 1;
+                }
+            }
+            else if(isPressqwe)
+            {
+                TimeShow.Interval = 1000;
+                if (qwe < ClickShow.Interval / 1000 + 2)
+                {
+                    label2.Text = (ClickShow.Interval / 1000 - qwe + 1).ToString();
+                    qwe++;
+                }
+                else
+                {
+                    qwe = 2;
+                    TimeShow.Interval = 1;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isPress)
+            {
+                tmrShow.Enabled = true;
+                TimeShow.Enabled = true;
+                isPress = true;
+                button1.Text = "Стоп";
+            }
+            else
+            {
+                tmrShow.Enabled = false;
+                isPress = false;
+                button1.Text = "Запись";
+                TimeShow.Enabled = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isPressqwe)
+            {
+                ClickShow.Enabled = false;
+                TimeShow.Enabled = false;
+                button2.Text = "Пуск";
+                isPressqwe = false;
+            }
+            else
+            {
+                TimeShow.Enabled = true;
+                button2.Text = "Стоп";
+                isPressqwe = true;
+
+                if (numbersX.Count > 0)
+                {
+                    if (isPress)
+                    {
+                        tmrShow.Enabled = false;
+                        isPress = false;
+                        button1.Text = "Запись";
+                        TimeShow.Enabled = false;
+                    }
+
+                    ClickOwer();
+                    ClickShow.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Сначала выполните запись!");
+                }
+            }
+        }
+
+        private void ClickShow_Tick(object sender, EventArgs e)
+        {
+            if (CountClick > 0)
+            {
+                ClickOwer();
+            }
+            else
+            {
+                ClickShow.Enabled = false;
+            }
+        }
+
+        void ClickOwer()
+        {
+            for (int i = 0; i < numbersX.Count; i++)
+            {
+                int x = numbersX.ElementAt(i);
+                int y = numbersY.ElementAt(i);
+
+                Cursor.Position = new Point(x, y);
+
+                var sim = new InputSimulator();
+                sim.Mouse.LeftButtonClick();
+                
+            }
+            CountClick--;
+            label3.Text = CountClick.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isPress)
+            {
+                tmrShow.Enabled = false;
+                isPress = false;
+                button1.Text = "Запись";
+                TimeShow.Enabled = false;
+            }
+
+            numbersX.Clear();
+            numbersY.Clear();
+            label1.Text = "";
+        }
+    }
+}
